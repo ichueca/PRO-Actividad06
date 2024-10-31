6e90d74e1d65bd2977867999b3856f4e7329c4ee
ichueca <inigo.chueca@zabalburu.org>
2024-10-31 13:31:09 +0100

    Creada Plantilla Actividad06

---
+++ Actividad06/src/main/java/org/zabalburu/actividad06/EnvioCorreos.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.zabalburu.actividad06;
+
+
+public class EnvioCorreos {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
